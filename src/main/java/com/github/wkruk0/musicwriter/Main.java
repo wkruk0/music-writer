c6808fd0f4c064993db7b2c5d8ac0af6ec43da87
Michal Nestorowicz <michal.nestorowicz@gmail.com>
2017-03-06 20:17:28 +0100

    Add basic gradle structure

---
+++ src/main/java/com/github/wkruk0/musicwriter/Main.java
@@ -0,0 +1,11 @@
+package com.github.wkruk0.musicwriter;
+
+public class Main {
+
+  public static String GREETING = "Hello world!";
+
+  public static void main(String[] args) {
+    System.out.println(GREETING);
+  }
+}
+
